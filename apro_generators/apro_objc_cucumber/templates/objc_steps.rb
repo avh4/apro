9e3338486dde3dc3ee46d768059def4690c19990
Aaron VonderHaar <gruen0aermel@gmail.com>
2009-03-05 17:01:14 -0800

    Create apro_objc_cucumber component generator.

---
+++ apro_generators/apro_objc_cucumber/templates/objc_steps.rb
@@ -0,0 +1,6 @@
+require "spec"
+require "osx/cocoa"
+$:.unshift File.dirname(__FILE__) + "/../../build/bundles"
+require "Application.bundle"
+
+# Add your steps here.  Run `rake features` to print out stubs for your step matchers.
